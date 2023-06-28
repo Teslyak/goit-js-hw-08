efc48ef3b2d8fe182c1a0167921fcababc024dea
Teslyak <124871929+Teslyak@users.noreply.github.com>
2023-06-28 13:06:01 +0000

    Deploying to gh-pages from @ Teslyak/goit-js-hw-08@ae8a62bd6bfbadacd3c3150d7e69e1e7d0d34ca7 🚀

--- index.0848b416.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello world"),console.log("object");
-//# sourceMappingURL=index.0848b416.js.map
--- index.33c8432a.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello world"),console.log("object");
-//# sourceMappingURL=index.33c8432a.js.map
