c724ea05c7cc0e71d3f9f23c6b3cd79ee5060ded
Teslyak <124871929+Teslyak@users.noreply.github.com>
2023-06-30 16:53:11 +0300

    hw8-task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,10 +2,22 @@ const refs = {
     form: document.querySelector('.feedback-form'),
 };
 
-console.log(refs.form);
-
 refs.form.addEventListener('input', onInputForm);
+ const obj = {
 
+    };
 function onInputForm(event) {
-  console.log(event.currentTarget);  
+   
+    const inputLabel = event.target.name;
+    const inputValue = event.target.value;
+    const obj = JSON.parse(localStorage.getItem('feedback-form-state')) ?? {};
+    console.log(obj);
+    if (inputLabel === "email") {
+        obj.email = inputValue;
+    } else if (inputLabel === "message") {
+         obj.message = inputValue;
+    }   
+    console.log(obj);
+    
+    localStorage.setItem('feedback-form-state', JSON.stringify(obj));
 };
