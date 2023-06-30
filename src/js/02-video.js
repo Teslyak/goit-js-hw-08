673097c5b1e7b6855d0b3140ac255d561a9cde57
Teslyak <124871929+Teslyak@users.noreply.github.com>
2023-06-30 15:28:37 +0300

    hw8-task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+const throttle = require('lodash.throttle');
+import Player from '@vimeo/player';
+const vimeoPlayer = document.querySelector('iframe')
+const player = new Player(vimeoPlayer);
+
+const onPlay = throttle(function(data) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data));
+    
+}, 1000);
+
+player.on('timeupdate', onPlay);
+
+const counter = JSON.parse(localStorage.getItem("videoplayer-current-time"));
+
+player.setCurrentTime(counter.seconds).then(function(seconds) {    
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,11 @@
+const refs = {
+    form: document.querySelector('.feedback-form'),
+};
 
+console.log(refs.form);
+
+refs.form.addEventListener('input', onInputForm);
+
+function onInputForm(event) {
+  console.log(event.currentTarget);  
+};
