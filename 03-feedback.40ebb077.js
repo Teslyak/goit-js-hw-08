bbb6141763fe843418614ea93d173c2b74ec86ae
Teslyak <124871929+Teslyak@users.noreply.github.com>
2023-06-30 12:29:23 +0000

    Deploying to gh-pages from @ Teslyak/goit-js-hw-08@673097c5b1e7b6855d0b3140ac255d561a9cde57 🚀

---
+++ 03-feedback.40ebb077.js
@@ -0,0 +1,2 @@
+const o={form:document.querySelector(".feedback-form")};console.log(o.form),o.form.addEventListener("input",(function(o){console.log(o.currentTarget)}));
+//# sourceMappingURL=03-feedback.40ebb077.js.map
---
+++ 03-feedback.555e5ac2.js
@@ -0,0 +1,2 @@
+!function(){var o={form:document.querySelector(".feedback-form")};console.log(o.form),o.form.addEventListener("input",(function(o){console.log(o.currentTarget)}))}();
+//# sourceMappingURL=03-feedback.555e5ac2.js.map
