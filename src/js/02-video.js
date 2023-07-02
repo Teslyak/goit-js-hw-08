d3eca4d1fa613508d3b75dac328cf7d996835170
Teslyak <124871929+Teslyak@users.noreply.github.com>
2023-07-02 16:07:44 +0300

    hw8-task2-finish

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,21 +4,10 @@ const vimeoPlayer = document.querySelector('iframe')
 const player = new Player(vimeoPlayer);
 
 const onPlay = throttle(function(data) {
-    localStorage.setItem("videoplayer-current-time", JSON.stringify(data));
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds));
     
 }, 1000);
 
 player.on('timeupdate', onPlay);
-
-const counter = JSON.parse(localStorage.getItem("videoplayer-current-time"));
-
-player.setCurrentTime(counter.seconds).then(function(seconds) {    
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            break;
-        default:
-            break;
-    }
-});
-
+const counter = JSON.parse(localStorage.getItem("videoplayer-current-time")) ?? 0;
+player.setCurrentTime(counter);
